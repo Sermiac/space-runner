aab49e8b5ab2fc1ac9c5528456dacc5d03d3b921
Sermiac <miguelo320484@gmail.com>
2025-12-14 21:01:29 -0500

    Added level selector
    Added level selector logic
    Changed Game tree organization

---
+++ Scripts/level_selector.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var levels = $MarginContainer/GridContainer
+
+
+func _ready() -> void:
+	create_levels_list()
+		
+
+func create_levels_list():
+	var dir = DirAccess.open("res://Scenes/levels/")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		
+		while file_name != "":
+			if not dir.current_is_dir():
+				create_level_button(file_name)
+			file_name = dir.get_next()
+			
+		dir.list_dir_end()
+
+func create_level_button(btn_name: StringName):
+	var btn = Button.new()
+	btn.text = btn_name.replace(".tscn", "").replace("le" , "Le").replace("_", " ")
+	btn.add_theme_font_size_override("font_size", 40)
+	btn.pressed.connect(on_level_button_pressed.bind(btn.text))
+	levels.add_child(btn)
+
+func on_level_button_pressed(btn_name: StringName):
+	btn_name = btn_name.replace(" ", "_")
+	var level = "res://Scenes/levels/%s.tscn" % btn_name.to_lower()
+	var level_scene = load(level).instantiate()
+	get_parent().add_child(level_scene)
+	self.call_deferred("queue_free")
